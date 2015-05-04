a7c0a3e11f005911fcf7732681af63f79c2d86f4
probe <re2rar@gmail.com>
2015-05-04 12:28:32 +0900

    Disable liveload

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -68,8 +68,8 @@ module.exports = function (grunt) {
       options: {
         port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: '0.0.0.0',
-        livereload: 35729
+        hostname: '0.0.0.0'
+       // livereload: 35729
       },
       proxies: [
         {
